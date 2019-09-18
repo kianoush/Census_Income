4ab2a66fd0490c2a6fa6d997a48c15d5076a724d
Kian1212 <k.karimipor@gmail.com>
2019-09-17 19:52:31 -0700

    Initial commit

---
+++ Census_Income.py
@@ -0,0 +1,19 @@
+"""
+Census Income
+Predict if an individual makes greater or less than $50000 per year
+"""
+
+"""
+Instances: 48842
+
+Attributes: 15
+
+Tasks: Classification
+
+Downloads: 1275
+
+Year Published: 1996
+
+Missing Values: Yes
+"""
+
