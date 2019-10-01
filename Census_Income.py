1e97c46511e757fbe5ac211e00bb5967f5443ed5
Kian1212 <k.karimipor@gmail.com>
2019-09-30 21:26:44 -0700

    Train Data

--- Census_Income.py
+++ Census_Income.py
@@ -10,8 +10,6 @@ Attributes: 15
 
 Tasks: Classification
 
-Downloads: 1275
-
 Year Published: 1996
 
 Missing Values: Yes
@@ -20,10 +18,8 @@ Missing Values: Yes
 import pandas as pd
 import torch
 import torch.nn as nn
-import missingno
-import matplotlib.pyplot as plt
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 
 
 """
@@ -32,7 +28,7 @@ Import data
 
 raw_data = pd.read_csv('census_income_dataset.csv')
 raw_data.info()
-print(raw_data.describe())
+#print(raw_data.describe())
 columns = raw_data.keys()
 # missingno.matrix(raw_data, figsize = (10,5))
 # plt.show()
@@ -40,12 +36,12 @@ columns = raw_data.keys()
 for column in columns:
     if raw_data[column].dtype == object:
         print(column, ':', len(np.unique(raw_data[column])))
-        print(np.unique(raw_data[column]), end='')
-    print()
+        #print(np.unique(raw_data[column]), end='')
+    #print()
 
 for column in columns:
     raw_data[column].replace(('?'), (np.nan), inplace=True)
-    print(column, " :", raw_data[column].isna().sum())
+    #print(column, " :", raw_data[column].isna().sum())
 
 raw_data['workclass'].fillna('no_fill', inplace=True)
 raw_data['occupation'].fillna('no_fill', inplace=True)
@@ -54,7 +50,7 @@ raw_data['occupation'].fillna('no_fill', inplace=True)
 raw_data['workclass'].replace(('no_fill', 'Federal-gov', 'Local-gov', 'Never-worked', 'Private', 'Self-emp-inc',
                             'Self-emp-not-inc', 'State-gov', 'Without-pay'), (0, 1, 2, 3, 4, 5, 6, 7, 8),
                             inplace =True)
-print(raw_data['education'].value_counts().sort_index())
+#print(raw_data['education'].value_counts().sort_index())
 raw_data['education'].replace(('10th', '11th', '12th', '1st-4th', '5th-6th', '7th-8th',
                             '9th'), ('school'),
                             inplace =True)
@@ -75,13 +71,81 @@ raw_data['occupation'].replace(('?', 'Adm-clerical', 'Armed-Forces', 'Craft-repa
                             'Tech-support', 'Transport-moving'), (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14),
                             inplace =True)
 
-raw_data['sex'] = np.where(raw_data.sex=='Male', 0, 1)
-raw_data['income_level'] = np.where(raw_data.income_level=='<=50K', 0, 1)
-
-d = (raw_data.hours_per_week.value_counts())
-
+raw_data['sex'] = np.where(raw_data.sex == 'Male', 0, 1)
+raw_data['income_level'] = np.where(raw_data.income_level == '<=50K', 0, 1)
+raw_data['hours_per_week'] = np.where(raw_data.hours_per_week < 40, 0, 1)
 
 raw_data.drop(columns=['fnlwgt', 'education_num',
        'race', 'capital_gain', 'capital_loss', 'native_country', 'relationship'], inplace=True)
+
+data = raw_data.iloc[:, :7]
+labels = raw_data.iloc[:, 7]
+
+
+"""
+Split train and test
+"""
+X_train, X_test, Y_train, Y_test = train_test_split(data, labels, shuffle=True, test_size=0.2)
+X_train01, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, shuffle=True, test_size=0.2)
+
+X_train = torch.tensor(X_train01).float()
+X_test = torch.tensor(X_test).float()
+X_valid = torch.tensor(X_valid).float()
+
+Y_train = torch.tensor(Y_train).long()
+Y_test = torch.tensor(Y_test).long()
+Y_valid = torch.tensor(Y_valid).long()
+
+"""
+Model
+"""
+num_samples = X_train.shape[0]
+num_classes = 2
+num_hiddenl = 10
+model = torch.nn.Sequential(torch.nn.Linear(num_samples, num_hiddenl),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(num_hiddenl, num_classes))
+
+"""
+Optim
+"""
+optim = torch.optim.Adam(model.parameters(), lr=0.001)
+
+"""
+Loss
+"""
+loss = torch.nn.CrossEntropyLoss()
+
+"""
+train
+"""
+
+num_sample_train = torch.tensor(X_train.shape[0])
+num_sample_test = torch.tensor(X_test.shape[0])
+num_sample_valid = torch.tensor(X_valid.shape[0])
+
+num_epochs = 200
+for epoch in range(num_epochs):
+    optim.zero_grad()
+    Y_pred = model(X_train)
+    loss_value = loss(Y_pred, Y_train)
+
+    num_corrects = torch.sum(torch.max(Y_pred, 1)[1] == Y_train)
+    acc_train = num_corrects.float() / float(num_sample_train)
+
+    loss_value.backward()
+    optim.step()
+
+    yp = model(X_valid)
+    num_corrects = torch.sum(torch.max(yp, 1)[1]==Y_valid)
+    acc_valid = num_corrects.float() / float(num_sample_valid)
+    print("Epoch: ", epoch, 'Train Loss: ', loss_value.item(), 'Train Accurecy: ', acc_train.item(), 'VALIDATION acoreccy', acc_valid.item())
+
+
+
+yp = model(X_test)
+num_corrects = torch.sum(torch.max(yp, 1)[1]==Y_test)
+acc_test = num_corrects.float() / float(num_sample_test)
+print('Test Accurecy: ', acc_test.item())
 
 print('End!!!')
